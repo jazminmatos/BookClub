a7bf540a575b17521576ff4683b13093af857ddd
jazminmatos <jmatos1993@gmail.com>
2021-04-30 04:47:26 -0400

    omniauth functionality using devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
     has_many :comments
     has_many :books
     has_many :clubs, through: :books
@@ -11,4 +13,11 @@ class User < ApplicationRecord
 
     validates :name, :username, presence: true
     validates :username, uniqueness: true
+
+    def self.create_from_provider_data(provider_data)
+      where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+        user.email = provider_data.info.email
+        user.password = Devise.friendly_token[0, 20]
+      end
+    end
 end
