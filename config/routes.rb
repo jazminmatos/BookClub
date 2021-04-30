1f240c2c8fa7f734d60cdd86d9006bfe7ac77efd
jazminmatos <jmatos1993@gmail.com>
2021-04-29 21:26:17 -0400

    you get a view file, everybody gets a view file

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   post "/logout", to: "sessions#destroy"
 
-  get "/welcome", to: "welcome#new"
+  get "/welcome", to: "welcome#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
