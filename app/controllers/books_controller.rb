2e51163406a4d8d3fd20881db03c6e7dff86f76f
jazminmatos <jmatos1993@gmail.com>
2021-05-02 03:12:27 -0400

    established comment instances

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
+    before_action :set_book, only: [:show, :edit, :update]
+    
     def index
+        @books = Book.all
     end
 
     def show
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,56 @@
 class CommentsController < ApplicationController
+    before_action :set_comment, only: [:show, :edit, :update]
+    
     def index
+        @comments = Comment.all
     end
 
     def show
     end
 
     def new
+        @comment = Comment.new
     end
 
     def create
+        comment = Comment.new(comment_params)
+
+        if comment.save
+            redirect_to comment_path(comment)
+            # Will want to change this to the comment's book page
+            # will need to set a book instance
+        else
+            render :new
+        end
     end
 
     def edit
     end
 
     def update
+        if @comment.update(comment_params)
+            redirect_to comment_path(@comment)
+            # Will want to change this to the comment's book page
+            # will need to set a book instance
+        else
+            render :edit
+        end
     end
 
     def destroy
+        Comment.find(params[:id]).destroy
+        redirect_to comments_path
+        # Will want to change this to the comment's book page
+        # will need to set a book instance
     end
 
     private
 
     def comment_params
         params.require(:comment).permit(:content)
+    end
+
+    def set_comment
+        @comment = Comment.find(params[:id])
     end
 end
