6e89ccf469cdfa8c841924fb229fd0f8d0d7c160
jazminmatos <jmatos1993@gmail.com>
2021-05-03 12:36:10 -0400

    updating nav bar links

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :clubs
 
   root to: 'welcome#index'
+  get '/about' => 'welcome#about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
