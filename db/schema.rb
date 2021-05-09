c4e4de8cdb7644717f32a299019378c1ca5f0a7a
jazminmatos <jmatos1993@gmail.com>
2021-05-09 16:40:40 -0400

    removed name & username from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_162630) do
+ActiveRecord::Schema.define(version: 2021_05_09_203845) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -50,8 +50,6 @@ ActiveRecord::Schema.define(version: 2021_04_30_162630) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 500, default: "", null: false
     t.string "email", default: "", null: false
